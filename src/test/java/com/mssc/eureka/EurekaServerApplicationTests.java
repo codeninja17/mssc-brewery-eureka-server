8b48b672cfd8dcf9fbb622485e0a7a59e64fcfff
codeninja17 <ankitkr09@gmail.com>
2021-06-15 19:59:01 +0530

    init

---
+++ src/test/java/com/mssc/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mssc.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
